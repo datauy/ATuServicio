406d357498af9fea3ee85fc6511903aeaf3dafe2
Lucia Escanellas <raviolicode@gmail.com>
2015-01-29 12:13:39 -0200

    Calculate averages

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,14 @@
 class Provider < ActiveRecord::Base
   has_many :sites, dependent: :delete_all
+
+  def average(name)
+    columns = METADATA[:precios][:averages][name][:columns]
+    values = columns.map do |column|
+      self.send(column.to_sym)
+    end
+
+    # I cannot average unless I have all the data
+    valid_values = values && !values.empty? && !values.include?(nil)
+    values.reduce(:+).to_f / values.size if valid_values
+  end
 end
