d529ee5aaabf82a7b2a7bd20d08f462e10af5da4
Lucia Escanellas <raviolicode@gmail.com>
2015-01-29 13:07:39 -0200

    Fix column names to symbols in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def columns_name_description(group)
-    group_names = METADATA[group]['description']
-    group_descriptions = METADATA[group]['columns']
+    group_names = METADATA[group][:description]
+    group_descriptions = METADATA[group][:columns]
 
     group_names.zip(group_descriptions)
   end
