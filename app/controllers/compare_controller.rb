ca862632325f319ce09b51409a3b90b44ffea54b
Lucia Escanellas <raviolicode@gmail.com>
2015-01-30 19:02:54 -0200

    Allow to go to the compare page with no providers selected

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -1,6 +1,6 @@
 class CompareController < ApplicationController
   def index
-    provider_ids = *params[:id].split("/")
+    provider_ids = *params[:id].try(:split, "/") || []
     @selected_providers = Provider.find(provider_ids.take(3))
     flash['alert'] = 'Solo se pueden elegir hasta 3 proveedores' if provider_ids.length > 3
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   match '/(departamento/:departamento)' => 'home#index', via: :get
-  match 'comparar/*id' => 'compare#index', via: :get, as: :compare
+  match 'comparar/(|*id)' => 'compare#index', via: :get, as: :compare
 end
