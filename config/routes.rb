3b08f92ba2b525782f7c5d3e879cc3165fdec311
Fernando Briano <fernando@picandocodigo.net>
2015-01-31 04:47:24 -0200

    Fix quienes somos route, add minimal styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   match '/(departamento/:departamento)' => 'home#index', via: :get
   get '/comparar' => 'compare#index', as: :compare
-  get '/quienes_somos' => 'home#quienes_somos'
+  get '/quienes_somos' => 'home#sobre_el_proyecto'
 end
