6deb8129bfe05c748c6ed793849d5896f7361488
Fernando Briano <fernando@picandocodigo.net>
2015-01-31 01:39:55 -0200

    Adds Quienes Somos page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   match '/(departamento/:departamento)' => 'home#index', via: :get
   get '/comparar' => 'compare#index', as: :compare
+  get '/quienes_somos' => 'home#quienes_somos'
 end
