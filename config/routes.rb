9d9026b1abd7797199e41e691b3fc992775cb3e4
Fernando Briano <fernando@picandocodigo.net>
2015-01-31 16:23:38 -0200

    Changes quienes somos to sobre el proyecto

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   match '/(departamento/:departamento)' => 'home#index', via: :get
   get '/comparar' => 'compare#index', as: :compare
-  get '/quienes_somos' => 'home#sobre_el_proyecto'
+  get '/sobre_el_proyecto' => 'home#sobre_el_proyecto'
 end
