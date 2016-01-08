4f9a1b77db6917de83ccd11a5aac7d9fea86c165
Fernando Briano <fernando@picandocodigo.net>
2016-01-08 16:49:29 -0300

    URL with id's for comparing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   match '/(departamento/:departamento)' => 'home#index', via: :get
-  get '/comparar' => 'compare#index', as: :compare
+  get '/comparar' => 'compare#index'
+  get '/comparar/:selected_providers' => 'compare#index', as: :compare
   get '/sobre_el_proyecto' => 'home#sobre_el_proyecto'
 end
