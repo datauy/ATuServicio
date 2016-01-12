2c6540a6204fbb8cc3a64753a1fbc9d0977b200b
Fernando Briano <fernando@picandocodigo.net>
2016-01-12 18:21:34 -0300

    Icons in group titles

--- app/controllers/compare_controller.rb
+++ app/controllers/compare_controller.rb
@@ -3,7 +3,19 @@ class CompareController < ApplicationController
   def index
     provider_ids = params[:selected_providers].try(:split, " ") || []
     @selected_providers = Provider.find(provider_ids.take(3))
+
     flash['alert'] = 'Solo se pueden elegir hasta 3 proveedores' if provider_ids.length > 3
+
+    @groups = {
+      estructura: 'estructura',
+      metas: 'metas',
+      precios: 'price',
+      tiempos_espera: 'calendar',
+      satisfaccion_derechos: 'derechos',
+      rrhh: 'rrhh',
+      solicitud_consultas: 'consultas'
+    }
+
     respond_to do |format|
       format.html
       format.js
