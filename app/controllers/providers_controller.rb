c1f2d3c04513c6a06bf9f80e506246fe47216df6
Fernando Briano <fernando@picandocodigo.net>
2016-01-15 15:48:08 -0300

    Remove unnecessary view

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,10 +1,3 @@
 class ProvidersController < ApplicationController
   autocomplete :provider, :search_name, full: true, limit: 15
-
-  def show
-    @providers = Provider.all.map(&:nombre_abreviado)
-    respond_to do |format|
-      format.json
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,4 @@ Rails.application.routes.draw do
   end
 
   get '/sobre_el_proyecto' => 'home#sobre_el_proyecto'
-
-  get '/providers' => 'providers#show'
 end
