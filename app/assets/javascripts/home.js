e358c7be7222e40f4a95f5b858ce5c99aaccb848
Fernando Briano <fernando@picandocodigo.net>
2016-01-21 18:15:01 -0300

    Unchecks a provider when it's removed from the selection menu

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,7 +10,9 @@ $(".add_compare").click(function(){
   };
   if ($(this).prop('checked') === true){
     if (selected_providers.length == 3){
-      selected_providers.pop();
+      var to_remove = selected_providers.pop();
+      var box = $(".checkbox").find("[data-id='" + to_remove.id + "']");
+      box.prop('checked', false);
     }
     selected_providers.push(provider);
   } else {
@@ -20,7 +22,7 @@ $(".add_compare").click(function(){
   selected_providers.map(function(provider){
     provider_names += '<div class="btn-tag">' +
       '<p>' + provider.name + '</p>' +
-      '<a href="#"><i class="fa fa-trash"></i></a>' +
+      '<a href="#"><i class="fa fa-trash" data-id="' + provider.id + '"></i></a>' +
       '</div>';
   });
   $('.provider-names').html(provider_names);
