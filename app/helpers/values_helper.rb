df5e8b8812bb468be6f26a9cad65b9d7dca5646c
Fernando Briano <fernando@picandocodigo.net>
2017-01-30 14:58:31 -0300

    Cambios texto ASSE modales

--- app/helpers/values_helper.rb
+++ app/helpers/values_helper.rb
@@ -60,7 +60,10 @@ module ValuesHelper
 
   def asse_waiting_times
     <<-eos
-    ASSE desarrolló en el año 2012  el Sistema de Gestión Consultas (SGC) (Sistema web), que se encuentra en proceso de expansión en todo el país, tanto en el primer, como segundo y tercer nivel de atención. Actualmente se encuentra en 221 Unidades Asistenciales, de las cuales 32 son Hospitales o Centros Auxiliares, 24 son Centros de Salud y 165 son Policlínicas. Se prevé que para finales del 2018 estará implantado en todo el país. Sin embargo, los datos de tiempo de espera que surgen actualmente del SGC no son representativos de toda la institución, ni de sus diferentes niveles de complejidad.<br>Ver información complementaria en<br><a
+    ASSE desarrolló en el año 2012 el Sistema de Gestión Consultas (SGC) (Sistema web), que se encuentra en proceso de expansión en todo el país, tanto en el primer, como segundo y tercer nivel de atención.<br><br>
+    Actualmente se encuentra implantado en 267 Unidades Asistenciales -46 unidades más que en el año 2015-, de las cuales 217 son Centros de Salud o Policlínicas y 50 son Hospitales o Centros Auxiliares.<br><br>
+    Está prevista que continúe la implantación del sistema a lo largo de los próximos años. En función de esta implantación progresiva, los datos de tiempo de espera que surgen actualmente del SGC no son representativos de toda la institución, ni de sus diferentes niveles de complejidad.<br><br>
+    Ver información complementaria en<br><a
     href="http://www.asse.com.uy/contenido/Movilidad-Regulada-8431" target="_blank">http://www.asse.com.uy/contenido/Movilidad-Regulada-8431</a>
     eos
   end
