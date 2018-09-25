e763ef10b9e8008f5f930eaae9814c520ef4ea70
Fernando Briano <fernando@picandocodigo.net>
2018-09-25 19:07:18 +0100

    Arreglo de viejo bicho

--- db/migrate/20170126042055_update_goals_columns.rb
+++ db/migrate/20170126042055_update_goals_columns.rb
@@ -1,7 +1,7 @@
 class UpdateGoalsColumns < ActiveRecord::Migration
   def change
     [:captacion_rn, :control_desarrollo, :control_embarazo, :control_hiv_vdrl, :control_pauta_45_64].each do |meta|
-      add_column :providers, a, :decimal unless column_exists?(:providers, meta)
+      add_column :providers, meta, :decimal unless column_exists?(:providers, meta)
     end
     add_column :providers, :indice_cesareas, :integer unless column_exists?(:providers, :indice_cesareas)
 
